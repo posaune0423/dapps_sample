d08cc23679591432a0100c6479bc873f53c27517
Asuma Yamada <asuma@linear-jp.biz>
2021-06-24 21:16:25 +0900

    refactor: make src dir

--- metamask/nuxt.config.js
+++ metamask/nuxt.config.js
@@ -16,6 +16,8 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  srcDir: 'src/',
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
