20467aef9fcdabda0bab539d39ef633326dd3655
Asuma Yamada <asuma@linear-jp.biz>
2021-06-25 01:05:26 +0900

    feat: create getter/setter of account store

--- metamask/src/composables/store/account.ts
+++ metamask/src/composables/store/account.ts
@@ -1,24 +1,39 @@
 import { reactive, InjectionKey, readonly } from '@nuxtjs/composition-api'
 
-
 interface AccountStore {
   state: {
     readonly account: string;
+    readonly signature: string;
   };
+  getAccount: () => string;
+  getSignature: () => string;
   setAccount: (account: string) => void;
+  setSignature: (signature: string) => void;
+}
+
+const state = reactive({
+  account: '',
+  signature: ''
+})
+const getAccount = () => {
+  return state.account
+}
+const getSignature = () => {
+  return state.signature
+}
+const setAccount = (account: string) => {
+  state.account = account
+}
+const setSignature = (signature: string) => {
+  state.signature = signature
 }
 
 export const AccountKey: InjectionKey<AccountStore> = Symbol('AccountKey')
 
-const state = reactive({
-  account: '',
-})
-
-const setAccount = (account: string) => {
-  state.account = account
-}
-
 export default {
   state: readonly(state),
+  getAccount,
+  getSignature,
   setAccount,
+  setSignature,
 }
