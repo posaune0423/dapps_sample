b5fefea1e4abf1fe36b8c4748b4598c26d436808
Asuma Yamada <asuma@linear-jp.biz>
2021-06-25 01:06:57 +0900

    fix: authenticate before vivsiting page

--- metamask/src/pages/index.vue
+++ metamask/src/pages/index.vue
@@ -1,27 +1,20 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col v-if="store.state.signature" cols="12" sm="8" md="6">
       <div class="text-center">
         <logo />
         <vuetify-logo />
       </div>
       <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
+        <v-card-title class="headline"> Welcome to the Vuetify + Nuxt.js template </v-card-title>
         <v-card-text>
           <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
+            Vuetify is a progressive Material Design component framework for Vue.js. It was designed
+            to empower developers to create amazing applications.
           </p>
           <p>
             For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://vuetifyjs.com" target="_blank" rel="noopener noreferrer">
               documentation </a
             >.
           </p>
@@ -48,26 +41,18 @@
             >.
           </p>
           <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
+            Thank you for developing with Vuetify and I look forward to bringing more exciting
+            features in the future.
           </p>
           <div class="text-xs-right">
             <em><small>&mdash; John Leider</small></em>
           </div>
           <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer">
             Nuxt Documentation
           </a>
           <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer">
             Nuxt GitHub
           </a>
         </v-card-text>
@@ -77,17 +62,45 @@
         </v-card-actions>
       </v-card>
     </v-col>
+    <div v-else class="text-center">
+      <v-progress-circular
+        indeterminate
+        size="120"
+        color="primary my-10"
+        style="margin: calc(50vh - 120px) auto"
+      ></v-progress-circular>
+    </div>
   </v-row>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent, inject, onBeforeMount, useRouter } from '@nuxtjs/composition-api'
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import { AccountKey } from '~/composables/store/account'
 
-export default {
+export default defineComponent({
   components: {
     Logo,
-    VuetifyLogo,
+    VuetifyLogo
   },
-}
+  setup() {
+    const router = useRouter()
+
+    const store = inject(AccountKey)
+    if (!store) {
+      throw new Error(`${AccountKey} is not provided`)
+    }
+
+    onBeforeMount(() => {
+      if (!store.state.signature) {
+        router.push('/login')
+      }
+    })
+
+    return {
+      store
+    }
+  }
+})
 </script>
